99ad5060247b98e3cf6436c821e04e3603e0fcde
Armandof-g <g.zuzdesign@gmail.com>
2026-08-28 16:04:32 -0600

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: './', // Ensures relative asset paths for 100% GitHub Pages compatibility
-})
